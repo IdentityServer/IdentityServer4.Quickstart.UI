fdcd297b514eab537ea64c261b599acf7314c1c3
Dominick Baier <dbaier@leastprivilege.com>
2016-09-14 17:28:49 +0200

    updates for rc1-update2

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,9 +1,11 @@
 ﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
+
 using IdentityModel;
 using IdentityServer4.Quickstart.UI.Models;
 using IdentityServer4.Services;
+using IdentityServer4.Services.InMemory;
 using Microsoft.AspNetCore.Http.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using System;
