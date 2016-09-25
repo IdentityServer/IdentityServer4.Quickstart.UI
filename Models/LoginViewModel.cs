77ae886c9f1c8e4a1e9a388768289f9a761b544b
Brock Allen <brockallen@gmail.com>
2016-09-25 10:17:10 -0400

    update quickstart UI code for client IdP restrictions

--- Models/LoginViewModel.cs
+++ Models/LoginViewModel.cs
@@ -1,33 +1,14 @@
 ﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
+
 using System.Collections.Generic;
-using Microsoft.AspNetCore.Http;
-using System.Linq;
 
 namespace IdentityServer4.Quickstart.UI.Models
 {
     public class LoginViewModel : LoginInputModel
     {
-        public LoginViewModel(HttpContext httpContext)
-        {
-            ExternalProviders = httpContext.Authentication.GetAuthenticationSchemes()
-                .Where(x => x.DisplayName != null)
-                .Select(x => new ExternalProvider {
-                    DisplayName = x.DisplayName,
-                    AuthenticationScheme = x.AuthenticationScheme
-                });
-        }
-
-        public LoginViewModel(HttpContext httpContext, LoginInputModel other)
-            : this(httpContext)
-        {
-            Username = other.Username;
-            Password = other.Password;
-            ReturnUrl = other.ReturnUrl;
-        }
-
-        public string ErrorMessage { get; set; }
+        public bool EnableLocalLogin { get; set; }
         public IEnumerable<ExternalProvider> ExternalProviders { get; set; }
     }
 
