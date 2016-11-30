c13b1efcdabb589fc7a12dc5abddea931bf1b755
Dominick Baier <dbaier@leastprivilege.com>
2016-11-30 14:39:52 +0100

    update get script

--- get.ps1
+++ get.ps1
@@ -8,10 +8,10 @@ if (!(Test-Path -Path Models))      { mkdir Models }
 if (!(Test-Path -Path Views))       { mkdir Views }
 if (!(Test-Path -Path wwwroot))     { mkdir wwwroot }
 
-copy .\ui\IdentityServer4.Quickstart.UI-dev\Controllers\* Controllers -recurse -force
-copy .\ui\IdentityServer4.Quickstart.UI-dev\Models\* Models -recurse -force
-copy .\ui\IdentityServer4.Quickstart.UI-dev\Views\* Views -recurse -force
-copy .\ui\IdentityServer4.Quickstart.UI-dev\wwwroot\* wwwroot -recurse -force
+copy .\ui\IdentityServer4.Quickstart.UI-release\Controllers\* Controllers -recurse -force
+copy .\ui\IdentityServer4.Quickstart.UI-release\Models\* Models -recurse -force
+copy .\ui\IdentityServer4.Quickstart.UI-release\Views\* Views -recurse -force
+copy .\ui\IdentityServer4.Quickstart.UI-release\wwwroot\* wwwroot -recurse -force
 
 del ui.zip
 del ui -recurse
