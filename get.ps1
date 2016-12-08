fdbba993367d56b57d36b8fa5c6e60938dc04e01
Brock Allen <brockallen@gmail.com>
2016-12-08 09:24:36 -0500

    add filters folder

--- get.ps1
+++ get.ps1
@@ -4,11 +4,13 @@ Invoke-WebRequest $source -OutFile ui.zip
 Expand-Archive ui.zip
 
 if (!(Test-Path -Path Controllers)) { mkdir Controllers }
+if (!(Test-Path -Path Filters))     { mkdir Filters }
 if (!(Test-Path -Path Models))      { mkdir Models }
 if (!(Test-Path -Path Views))       { mkdir Views }
 if (!(Test-Path -Path wwwroot))     { mkdir wwwroot }
 
 copy .\ui\IdentityServer4.Quickstart.UI-release\Controllers\* Controllers -recurse -force
+copy .\ui\IdentityServer4.Quickstart.UI-release\Filters\* Filters -recurse -force
 copy .\ui\IdentityServer4.Quickstart.UI-release\Models\* Models -recurse -force
 copy .\ui\IdentityServer4.Quickstart.UI-release\Views\* Views -recurse -force
 copy .\ui\IdentityServer4.Quickstart.UI-release\wwwroot\* wwwroot -recurse -force
