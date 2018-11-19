bd778812522b60d84a8cff2c56aa2cd17fb9c46d
Dominick Baier <dbaier@leastprivilege.com>
2018-11-19 10:02:33 +0100

    Update getmaster.ps1

--- getmaster.ps1
+++ getmaster.ps1
@@ -8,9 +8,9 @@ if (!(Test-Path -Path Quickstart))  { mkdir Quickstart }
 if (!(Test-Path -Path Views))       { mkdir Views }
 if (!(Test-Path -Path wwwroot))     { mkdir wwwroot }
 
-copy .\ui\IdentityServer4.Quickstart.UI-dev\Quickstart\* Quickstart -recurse -force
-copy .\ui\IdentityServer4.Quickstart.UI-dev\Views\* Views -recurse -force
-copy .\ui\IdentityServer4.Quickstart.UI-dev\wwwroot\* wwwroot -recurse -force
+copy .\ui\IdentityServer4.Quickstart.UI-master\Quickstart\* Quickstart -recurse -force
+copy .\ui\IdentityServer4.Quickstart.UI-master\Views\* Views -recurse -force
+copy .\ui\IdentityServer4.Quickstart.UI-master\wwwroot\* wwwroot -recurse -force
 
 del ui.zip
 del ui -recurse
